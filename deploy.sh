fd54caef2e3e0b2668aefb3dfd445367ce3c1ffe
Will Grant <will.granty@yahoo.co.uk>
2026-03-13 12:16:32 +0000

    Remove `green_gecko_garden_room.webp` asset: unused and no longer required

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,1 @@
-rsync -avz --chmod=D755,F644 --exclude 'deploy.sh' --exclude '.git' --exclude '.idea' --exclude '.gitignore' -e ssh ./ will@142.93.136.223:/var/www/loftsgreengecko/
+rsync -avz --delete --chmod=D755,F644 --exclude 'deploy.sh' --exclude '.git' --exclude '.idea' --exclude '.gitignore' --exclude '.gemini' --exclude 'img/not-used' --exclude 'done' --exclude 'planning' -e ssh ./ will@142.93.136.223:/var/www/loftsgreengecko/
